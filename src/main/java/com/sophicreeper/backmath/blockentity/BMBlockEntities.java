d7da513cb88040c903acb1fff8011dda9016e0d1
Fabricio20106 <85804854+Fabricio20106@users.noreply.github.com>
2025-01-05 23:03:22 -0300

    Added all barrels to `bm_barrel` block entity

--- src/main/java/com/sophicreeper/backmath/blockentity/BMBlockEntities.java
+++ src/main/java/com/sophicreeper/backmath/blockentity/BMBlockEntities.java
@@ -13,7 +13,7 @@ public class BMBlockEntities {
     public static final DeferredRegister<TileEntityType<?>> BLOCK_ENTITIES = DeferredRegister.create(ForgeRegistries.TILE_ENTITIES, BackMath.MOD_ID);
 
     public static final RegistryObject<TileEntityType<BMBarrelBlockEntity>> BM_BARREL = BLOCK_ENTITIES.register("bm_barrel", () -> TileEntityType.Builder.of(BMBarrelBlockEntity::new,
-            BMBlocks.ALJANWOOD_BARREL.get()).build(null));
+            BMBlocks.ALJANWOOD_BARREL.get(), BMBlocks.ALJANCAP_BARREL.get(), BMBlocks.INSOMNIAN_BARREL.get(), BMBlocks.AVONDALIC_WILLOW_BARREL.get()).build(null));
     public static final RegistryObject<TileEntityType<HeadBlockEntity>> HEAD = BLOCK_ENTITIES.register("head", () -> TileEntityType.Builder.of(HeadBlockEntity::new,
             BMBlocks.ANGRY_SOPHIE_HEAD.get(), BMBlocks.ANGRY_SOPHIE_WALL_HEAD.get(), BMBlocks.INSOMNIA_SOPHIE_HEAD.get(), BMBlocks.INSOMNIA_SOPHIE_WALL_HEAD.get(),
             BMBlocks.QUEEN_LUCY_HEAD.get(), BMBlocks.QUEEN_LUCY_WALL_HEAD.get(), BMBlocks.ZOMBIE_FABRICIO_HEAD.get(), BMBlocks.ZOMBIE_FABRICIO_WALL_HEAD.get())
